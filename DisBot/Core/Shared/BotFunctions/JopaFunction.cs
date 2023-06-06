ad8cc326bf4ebe44e35c2ffb6c68ead52b9833ec
GnomBober <energiguy@gmail.com>
2023-06-06 18:05:32 +0300

    jopa fixed#

--- DisBot/Core/Shared/BotFunctions/JopaFunction.cs
+++ DisBot/Core/Shared/BotFunctions/JopaFunction.cs
@@ -1,16 +1,10 @@
 ﻿using DisBot.Abstractrions.Interfaces;
 using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DisBot.Core.Shared.BotFunctions
 {
      class JopaFunction : IBotFunction
     {
-        public void JopaFunctione() { }
         public void Subscribe(ref DiscordSocketClient _client) { }
         public void Unsubscribe(ref DiscordSocketClient _client) { }
     }
